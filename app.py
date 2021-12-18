31d9423a4510ca4e6776f2bde4771bdc66190616
Shane Oh <ohshane71@gmail.com>
2021-12-18 23:04:01 +0900

    added style

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision.transforms import transforms
-# from torchsummary import summary
-
-from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM
-from pytorch_grad_cam.utils.image import show_cam_on_image
 
 from annotation import detectAndDisplay
 from model.FER2013_VGG19.VGG import VGG
@@ -25,25 +21,14 @@ app = Flask(__name__)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = VGG('VGG19')
 checkpoint = Path(__file__).parent / "model" / "FER2013_VGG19" / "PrivateTest_model.t7"
-model.load_state_dict(torch.load(checkpoint, map_location=device), strict=False)
+model.load_state_dict(torch.load(checkpoint, map_location="cpu"), strict=False)
 trans = transforms.Compose([
     transforms.Resize((48, 48)),   
     transforms.ToTensor(),
 ])
 
-target_layer = model.features.__getitem__(0)
-
-gradcam = GradCAM(model=model, target_layer=target_layer, use_cuda=True)
-scorecam = ScoreCAM(model=model, target_layer=target_layer, use_cuda=True)
-gradcamplusplus = GradCAMPlusPlus(model=model, target_layer=target_layer, use_cuda=True)
-ablationcam = AblationCAM(model=model, target_layer=target_layer, use_cuda=True)
-xgradcam = XGradCAM(model=model, target_layer=target_layer, use_cuda=True)
-eigencam = EigenCAM(model=model, target_layer=target_layer, use_cuda=True)
-cams = [gradcam, scorecam, gradcamplusplus, ablationcam, xgradcam, eigencam]
-
 uri = None
 name = None
-noise = False
 
 camera = cv2.VideoCapture(uri)
 # uri: http://192.168.0.44:81/stream
@@ -60,95 +45,73 @@ transform_test = transforms.Compose([
 ])
 
 net = VGG('VGG19')
-checkpoint = torch.load(root_dir / 'model' / 'FER2013_VGG19' / 'PrivateTest_model.t7')
+checkpoint = torch.load(root_dir / 'model' / 'FER2013_VGG19'
+        / 'PrivateTest_model.t7', map_location="cpu")
 net.load_state_dict(checkpoint['net'])
-net.cuda()
+# net.cuda()
 net.eval()
-# print(summary(net, (3, 44, 44)))
 
 frames = []
 temp_frame = None
 
 def gen_frames(camera):
-    # for cv2.putText
-    font = cv2.FONT_HERSHEY_SIMPLEX     
-    org = [10, 30]
-    fontScale = 0.7
-    text_color = (255, 255, 255)
-    face_border_color = (255, 255, 255)
-    thickness = 2
-
     if not camera.isOpened:
         print('--(!)Error opening video capture')
         exit(0)
     while True:
-        # time.sleep(1)
+        time.sleep(0.05)
         success, frame = camera.read()
         if not success:
             print('--(!) No captured frame -- Break!')
             break
 
-        frame, faceROI, faces = detectAndDisplay(frame, noise=noise)
+        temp_frame = frame
+
+        frame, faceROI, faces = detectAndDisplay(frame)
         if faceROI is not None:
-            gradcam_input = None
-            (x,y,w,h) = faces[0]
-            
             faceROI = cv2.resize(faceROI, dsize=(48, 48), interpolation=cv2.INTER_LINEAR).astype(np.uint8)
             faceROI = faceROI[:, :, np.newaxis]
             faceROI = np.concatenate((faceROI, faceROI, faceROI), axis=2)
+            cv2.imwrite('frame.jpg', faceROI)
             faceROI = Image.fromarray(faceROI)
             inputs = transform_test(faceROI)
 
-            ncrops, channel, height, width = np.shape(inputs)
+            ncrops, c, h, w = np.shape(inputs)
 
-            inputs = inputs.view(-1, channel, height, width)
-            inputs = inputs.cuda()
+            inputs = inputs.view(-1, c, h, w)
+            # inputs = inputs.cuda()
             inputs = Variable(inputs, volatile=True)
             outputs = net(inputs)
             outputs_avg = outputs.view(ncrops, -1).mean(0)
-
-            # grayscale_cam = gradcam(input_tensor=inputs)
-            # grayscale_cam = grayscale_cam[0, :]
-
-            # visualization_cam = show_cam_on_image(inputs, grayscale_cam)
-            # print(visualization_cam)
 
             score = F.softmax(outputs_avg)
             _, predicted = torch.max(outputs_avg.data, 0)
             score = score.tolist()
             score = list(map(lambda x: '{:.3f}'.format(x), score))
 
-            score_dict = dict(zip(class_names, score))
-
-            # 3-class
-            temp_dict = {}
-            temp_dict['Happy'] = score_dict['Happy']
-            temp_dict['Neutral'] = score_dict['Neutral']
-            temp_dict['Others'] = '{:.3f}'.format(1 - (float(score_dict['Happy']) + float(score_dict['Neutral'])))
-            score_dict = temp_dict
+            # font
+            font = cv2.FONT_HERSHEY_SIMPLEX
             
-
-            for key in score_dict.keys():
-                if class_names[predicted] == key:
-                    text_color = (50, 255, 50)
-                elif key == 'Others' and class_names[predicted] in ['Angry', 'Disgust', 'Fear', 'Sad', 'Surprise']:
-                    text_color = (50, 255, 50)
-                cv2.putText(frame, key, org, font, fontScale, text_color, thickness, cv2.LINE_AA)
-                org[0] = 110
-                cv2.putText(frame, score_dict[key], org, font, fontScale, text_color, thickness, cv2.LINE_AA)
-                cv2.rectangle(frame, (org[0]+90, org[1]-15), (org[0]+190, org[1]), (255,255,255), 2)
-                cv2.rectangle(frame, (org[0]+90, org[1]-15), (org[0]+90+int(float(score_dict[key])*100), org[1]), (255,255,255), -1)
-                score_dict[key] * 100
-                org[1] += 30
-                org[0] = 10
-                text_color = (255, 255, 255)
-            org = [10, 30]
+            # org
+            org = (10, 100)
+            
+            # fontScale
+            fontScale = 1
+            
+            # Blue color in BGR
+            color = (255, 255, 255)
+            
+            # Line thickness of 2 px
+            thickness = 2
 
             if class_names[predicted] == 'Happy':
-                face_border_color = (50, 255, 50)
+                frames.append(temp_frame)
+                color = (50, 255, 50)
 
-            frame = cv2.rectangle(frame, (x, y), (x+w, y+h), face_border_color, 1)
-            face_border_color = (255, 255, 255)
+            
+            cv2.putText(frame, ' '.join(score), org, font, fontScale, color, thickness, cv2.LINE_AA)
+
+            # print(f"score: {score}\npredicted: {class_names[predicted]}")
 
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
@@ -164,14 +127,11 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        global uri, name, noise
+        global uri, name
         uri = request.values.get('uri')
         name = request.values.get('name')
-        noise = request.form.get('noise')
         if uri == '0':
             uri = 0
-        if noise == 'off':
-            noise = False
         return redirect('/')
     return render_template('register.html')
 
--- app_grad.py
+++ app_grad.py
@@ -12,6 +12,10 @@ import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision.transforms import transforms
+# from torchsummary import summary
+
+from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
 
 from annotation import detectAndDisplay
 from model.FER2013_VGG19.VGG import VGG
@@ -27,8 +31,19 @@ trans = transforms.Compose([
     transforms.ToTensor(),
 ])
 
+target_layer = model.features.__getitem__(0)
+
+gradcam = GradCAM(model=model, target_layer=target_layer, use_cuda=True)
+scorecam = ScoreCAM(model=model, target_layer=target_layer, use_cuda=True)
+gradcamplusplus = GradCAMPlusPlus(model=model, target_layer=target_layer, use_cuda=True)
+ablationcam = AblationCAM(model=model, target_layer=target_layer, use_cuda=True)
+xgradcam = XGradCAM(model=model, target_layer=target_layer, use_cuda=True)
+eigencam = EigenCAM(model=model, target_layer=target_layer, use_cuda=True)
+cams = [gradcam, scorecam, gradcamplusplus, ablationcam, xgradcam, eigencam]
+
 uri = None
 name = None
+noise = False
 
 camera = cv2.VideoCapture(uri)
 # uri: http://192.168.0.44:81/stream
@@ -49,67 +64,91 @@ checkpoint = torch.load(root_dir / 'model' / 'FER2013_VGG19' / 'PrivateTest_mode
 net.load_state_dict(checkpoint['net'])
 net.cuda()
 net.eval()
+# print(summary(net, (3, 44, 44)))
 
 frames = []
 temp_frame = None
 
 def gen_frames(camera):
+    # for cv2.putText
+    font = cv2.FONT_HERSHEY_SIMPLEX     
+    org = [10, 30]
+    fontScale = 0.7
+    text_color = (255, 255, 255)
+    face_border_color = (255, 255, 255)
+    thickness = 2
+
     if not camera.isOpened:
         print('--(!)Error opening video capture')
         exit(0)
     while True:
-        time.sleep(0.05)
+        # time.sleep(1)
         success, frame = camera.read()
         if not success:
             print('--(!) No captured frame -- Break!')
             break
 
-        temp_frame = frame
-
-        frame, faceROI, faces = detectAndDisplay(frame)
+        frame, faceROI, faces = detectAndDisplay(frame, noise=noise)
         if faceROI is not None:
+            gradcam_input = None
+            (x,y,w,h) = faces[0]
+            
             faceROI = cv2.resize(faceROI, dsize=(48, 48), interpolation=cv2.INTER_LINEAR).astype(np.uint8)
             faceROI = faceROI[:, :, np.newaxis]
             faceROI = np.concatenate((faceROI, faceROI, faceROI), axis=2)
             faceROI = Image.fromarray(faceROI)
             inputs = transform_test(faceROI)
 
-            ncrops, c, h, w = np.shape(inputs)
+            ncrops, channel, height, width = np.shape(inputs)
 
-            inputs = inputs.view(-1, c, h, w)
+            inputs = inputs.view(-1, channel, height, width)
             inputs = inputs.cuda()
             inputs = Variable(inputs, volatile=True)
             outputs = net(inputs)
             outputs_avg = outputs.view(ncrops, -1).mean(0)
+
+            # grayscale_cam = gradcam(input_tensor=inputs)
+            # grayscale_cam = grayscale_cam[0, :]
+
+            # visualization_cam = show_cam_on_image(inputs, grayscale_cam)
+            # print(visualization_cam)
 
             score = F.softmax(outputs_avg)
             _, predicted = torch.max(outputs_avg.data, 0)
             score = score.tolist()
             score = list(map(lambda x: '{:.3f}'.format(x), score))
 
-            # font
-            font = cv2.FONT_HERSHEY_SIMPLEX
+            score_dict = dict(zip(class_names, score))
+
+            # 3-class
+            temp_dict = {}
+            temp_dict['Happy'] = score_dict['Happy']
+            temp_dict['Neutral'] = score_dict['Neutral']
+            temp_dict['Others'] = '{:.3f}'.format(1 - (float(score_dict['Happy']) + float(score_dict['Neutral'])))
+            score_dict = temp_dict
             
-            # org
-            org = (10, 100)
-            
-            # fontScale
-            fontScale = 1
-            
-            # Blue color in BGR
-            color = (255, 255, 255)
-            
-            # Line thickness of 2 px
-            thickness = 2
+
+            for key in score_dict.keys():
+                if class_names[predicted] == key:
+                    text_color = (50, 255, 50)
+                elif key == 'Others' and class_names[predicted] in ['Angry', 'Disgust', 'Fear', 'Sad', 'Surprise']:
+                    text_color = (50, 255, 50)
+                cv2.putText(frame, key, org, font, fontScale, text_color, thickness, cv2.LINE_AA)
+                org[0] = 110
+                cv2.putText(frame, score_dict[key], org, font, fontScale, text_color, thickness, cv2.LINE_AA)
+                cv2.rectangle(frame, (org[0]+90, org[1]-15), (org[0]+190, org[1]), (255,255,255), 2)
+                cv2.rectangle(frame, (org[0]+90, org[1]-15), (org[0]+90+int(float(score_dict[key])*100), org[1]), (255,255,255), -1)
+                score_dict[key] * 100
+                org[1] += 30
+                org[0] = 10
+                text_color = (255, 255, 255)
+            org = [10, 30]
 
             if class_names[predicted] == 'Happy':
-                frames.append(temp_frame)
-                color = (50, 255, 50)
+                face_border_color = (50, 255, 50)
 
-            
-            cv2.putText(frame, ' '.join(score), org, font, fontScale, color, thickness, cv2.LINE_AA)
-
-            # print(f"score: {score}\npredicted: {class_names[predicted]}")
+            frame = cv2.rectangle(frame, (x, y), (x+w, y+h), face_border_color, 1)
+            face_border_color = (255, 255, 255)
 
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
@@ -125,11 +164,14 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        global uri, name
+        global uri, name, noise
         uri = request.values.get('uri')
         name = request.values.get('name')
+        noise = request.form.get('noise')
         if uri == '0':
             uri = 0
+        if noise == 'off':
+            noise = False
         return redirect('/')
     return render_template('register.html')
 
